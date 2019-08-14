93da31e504f35261763271e403963f48e5066df6
Ting Ting Ye <tingtingye24@gmail.com>
2019-08-14 22:05:12 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,5 +1,9 @@
 # My Code here....
 def map_to_negativize (array)
-  
-  
+  count = 0
+  newarray =[]
+  while array[count]
+  newarray << array[count] -(2 * array[count])
+end
+  return newarray
 end
